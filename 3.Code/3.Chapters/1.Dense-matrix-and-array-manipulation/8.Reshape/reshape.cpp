53b38d5e0baac9f66b27f627cd29a90ff9767f15
vistar <1055311345@qq.com>
2023-04-04 23:50:08 +0800

    code of reshape inplace

--- 3.Code/3.Chapters/1.Dense-matrix-and-array-manipulation/8.Reshape/reshape.cpp
+++ 3.Code/3.Chapters/1.Dense-matrix-and-array-manipulation/8.Reshape/reshape.cpp
@@ -6,19 +6,33 @@ using namespace Eigen;
 
 int main()
 {
-    // 2D reshape
-    Matrix4i m = Matrix4i::Random();
-    cout << "Here is the matrix m:" << endl
-         << m << endl;
-    cout << "Here is m.reshaped(2, 8):" << endl
-         << m.reshaped(2, 8) << endl;
+     Matrix4i m = Matrix4i::Random();
 
-    cout << endl;
-    // 1D reshape
-    cout << "Here is the matrix m:" << endl
-         << m << endl;
-    cout << "Here is m.reshaped().transpose():" << endl
-         << m.reshaped().transpose() << endl;
-    cout << "Here is m.reshaped<RowMajor>().transpose():  " << endl
-         << m.reshaped<RowMajor>().transpose() << endl;
+     // 2D reshape
+     cout << "2D reshape:" << endl;
+     cout << "Here is the matrix m:" << endl
+          << m << endl;
+     cout << "Here is m.reshaped(2, 8):" << endl
+          << m.reshaped(2, 8) << endl;
+
+     cout << endl;
+     // 1D reshape
+     cout << "1D reshape:" << endl;
+     cout << "Here is the matrix m:" << endl
+          << m << endl;
+     cout << "Here is m.reshaped().transpose():" << endl
+          << m.reshaped().transpose() << endl;
+     cout << "Here is m.reshaped<RowMajor>().transpose():  " << endl
+          << m.reshaped<RowMajor>().transpose() << endl;
+
+     cout << endl;
+     // reshape in place
+     cout << "reshape in place:" << endl;
+     cout << "Here is the matrix m:" << endl
+          << m << endl;
+     cout << "Here is m.reshaped(2, 8):" << endl
+          << m.reshaped(2, 8) << endl;
+     m.resize(2, 8);
+     cout << "Here is the matrix m after m.resize(2,8):" << endl
+          << m << endl;
 }
